b1924624f23ed430761d641fe75c925ab9d41f23
Secqin <zhige311@hotmail.com>
2023-03-30 10:51:47 +0800

    添加mybatisplus配置和前后端交互Result文件配置

---
+++ springboot/src/main/java/com/secqin/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.secqin.controller;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.secqin.config.Result;
+import com.secqin.entity.User;
+import com.secqin.mapper.UserMapper;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Resource
+    UserMapper userMapper;
+    @GetMapping
+    public Result<?> findPage(@RequestParam(defaultValue = "1") Integer currentPage,
+                              @RequestParam(defaultValue = "10") Integer pageSize,
+                              @RequestParam(defaultValue = "test") String keyWord) {
+        Page<User> userPage = userMapper.selectPage(new Page<>(currentPage, pageSize), Wrappers.<User>lambdaQuery().like(User::getUsername, keyWord));
+        return Result.succes(userPage);
+    }
+
+    @PostMapping
+    public Result<?> save(@RequestBody User user) {
+        userMapper.insert(user);
+        Page<User> userPage = userMapper.selectPage(new Page<>(1, 1), Wrappers.<User>lambdaQuery().eq(User::getUsername, user.getUsername()));
+        return Result.succes(userPage);
+    }
+}
---
+++ springboot/src/main/java/com/secqin/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.secqin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.secqin.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}
