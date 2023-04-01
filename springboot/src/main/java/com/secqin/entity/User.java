790ede458a0ebd8789c83322baa4285a18dbf7eb
Secqin <zhige311@hotmail.com>
2023-04-01 21:53:13 +0800

    对User实体添加新的属性

--- springboot/src/main/java/com/secqin/entity/User.java
+++ springboot/src/main/java/com/secqin/entity/User.java
@@ -3,7 +3,10 @@ package com.secqin.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @TableName("User")
 @Data
@@ -13,4 +16,12 @@ public class User {
     private String username;
     private String password;
     private Integer role; // -1代表未登录 0代表普通用户 1代表工作人员 2代表超级管理员
+    private String phone;
+    private String email;
+    private Integer cond;
+    private Integer room;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime inTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime outTime;
 }
