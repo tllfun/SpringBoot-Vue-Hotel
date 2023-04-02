ed06b6ba702962ccb9c329114b42b39f3caebd77
goner <1577227473@qq.com>
2023-04-02 20:37:32 +0800

    全局使用图标

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,5 +4,12 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
+import * as ELIcons from '@element-plus/icons-vue'
 
-createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
+const app = createApp(App);
+app.use(store).use(router).use(ElementPlus).mount('#app')
+
+//全局使用图标
+for (let iconName in ELIcons) {
+    app.component(iconName, ELIcons[iconName])
+}
