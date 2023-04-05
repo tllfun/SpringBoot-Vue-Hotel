fd8e294f753938da0376c7fba815f4bf61d09e7c
Secqin <zhige311@hotmail.com>
2023-04-05 11:00:34 +0800

    rename ordersController

--- springboot/src/main/java/com/secqin/controller/OrdersController.java
+++ springboot/src/main/java/com/secqin/controller/OrdersController.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/orders")
-public class OrderController {
+public class OrdersController {
     @Resource
     OrdersMapper ordersMapper;
 
